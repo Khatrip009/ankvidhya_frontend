78d42abe1680a5c90ef2654a4e35d6051d24fc93
Khatrip009 <Khatrip.009@gmail.com>
2025-12-04 16:33:17 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const isGhPages = process.env.DEPLOY_TARGET === 'gh-pages' || mode === 'production';
-  return {
-    plugins: [react()],
-    // safer: relative paths so gh-pages can serve from any folder
-    base: isGhPages ? './' : '/',
-    build: {
-      chunkSizeWarningLimit: 1500,
-      
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: "/",   // CUSTOM DOMAIN MUST ALWAYS USE "/"
+  build: {
+    chunkSizeWarningLimit: 1500
+  }
 });
